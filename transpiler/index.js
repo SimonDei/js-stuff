49a56c0d2cd8342221153fd5109867ca19a8e21d
SimonDei <s.f.deitermann@gmail.com>
2024-03-22 10:21:15 +0100

    Update index.js

--- transpiler/index.js
+++ transpiler/index.js
@@ -1,25 +1,25 @@
 import Scanner from './tokenizer.js';
 import Parser from './parser.js';
 import Walker from './walker.js';
-import * as util from 'util';
 import * as fs from 'fs';
+import * as util from 'util';
 
-const code = fs.readFileSync('./index.bs', { encoding: 'utf-8' });
+const code = fs.readFileSync('./index.b', { encoding: 'utf-8' });
 
 const scanner = new Scanner();
 const tokens = scanner.tokenize(code);
 
 console.log(tokens);
-scanner.writeTokens();
+
+fs.writeFileSync('./tokens.txt', tokens.reduce((acc, tok) => acc + `{ type: '${tok.type}'${tok.value ? `, value: '${tok.value }'` : ''} },\n`, ''), { encoding: 'utf-8' });
 
 const parser = new Parser(tokens);
 const { ast } = parser.parse('EOF');
-parser.writeAst();
 
-console.log(util.inspect(ast, true, 100, true));
+console.log(util.inspect(ast, false, { depth: 100 }));
 
 const walker = new Walker(ast);
 walker.walk();
 walker.addStdFunctions();
 
-walker.writeSource();
+fs.writeFileSync('./source.js', walker.getSource(), { encoding: 'utf-8' });
