b9237725f97ece91fd68955bab03c20fc355e524
SimonDei <s.f.deitermann@gmail.com>
2024-03-22 10:22:15 +0100

    Update walker.js

--- transpiler/walker.js
+++ transpiler/walker.js
@@ -1,8 +1,25 @@
-import * as fs from 'fs';
 import StdDecls from './stdfunctions.js';
 
 export default class Walker {
   static #STDFUNCTIONS = Object.keys(StdDecls);
+
+  static #NATIVE_TYPES = {
+    'nothing': 'void',
+    'real': 'number',
+    'integer': 'number',
+    'string': 'string',
+    'boolean': 'boolean',
+    'element': 'HTMLElement'
+  };
+
+  static #NATIVE_OPERATORS = {
+    '.': '.',
+    '->': '.',
+    '==': '===',
+    'is': '===',
+    '!=': '!==',
+    'not': '!=='
+  };
 
   #ast = [];
   #prevTargetExpr = {};
@@ -30,8 +47,43 @@ export default class Walker {
     }
   }
 
-  globalsDeclaration() {
+  #addJsDocType(declType) {
+    let typeValue = ''
 
+    if (Object.keys(Walker.#NATIVE_TYPES).includes(declType)) {
+      typeValue = Walker.#NATIVE_TYPES[declType];
+    } else {
+      typeValue = declType;
+    }
+
+    return `/** @type {${typeValue}} */`;
+  }
+
+  #addJsDocReturnType(returnType) {
+    let typeValue = ''
+
+    if (Object.keys(Walker.#NATIVE_TYPES).includes(returnType)) {
+      typeValue = Walker.#NATIVE_TYPES[returnType];
+    } else {
+      typeValue = returnType;
+    }
+
+    return `/** @returns {${typeValue}} */`;
+  }
+
+  globalsDeclaration() {
+    if (this.#allowSpaces) {
+      this.#addSpaces();
+    }
+
+    const prevTargetExpr = this.#targetExpr;
+    this.#targetExpr = this.#targetExpr.body;
+
+    this.walk();
+
+    this.#targetExpr = prevTargetExpr;
+
+    this.#source += '\n';
   }
 
   functionDeclaration() {
@@ -39,7 +91,7 @@ export default class Walker {
       this.#addSpaces();
     }
 
-    this.#source += `/** @returns {${this.#targetExpr.returns === 'nothing' ? 'void' : this.#targetExpr.returns}} */ `;
+    // this.#source += `${this.#addJsDocReturnType(this.#targetExpr.returns)} `;
 
     if (this.#targetExpr.async) {
       this.#source += 'async ';
@@ -48,14 +100,14 @@ export default class Walker {
     this.#source += `function ${this.#targetExpr.name}(`;
 
     this.#targetExpr.parameters
-      .filter(e => e !== 'nothing')
+      // .filter(e => e.value !== 'nothing')
       .forEach((e, i, arr) => {
         if (i < arr.length - 1) {
-          this.#source += `${e}, `;
+          this.#source += `${e}, `; // `${this.#addJsDocType(e.type)} ${e.value}, `;
           return;
         }
 
-        this.#source += e;
+        this.#source += e; // `${this.#addJsDocType(e.type)} ${e.value}`;
       });
 
     this.#source += ') {\n';
@@ -91,13 +143,30 @@ export default class Walker {
   variableDeclaration() {
     this.#addSpaces();
 
-    if (this.#targetExpr.mutable) {
-      this.#source += 'let ';
-    } else {
-      this.#source += 'const ';
-    }
+    this.#source += `let ${this.#targetExpr.name} = `; //`let ${this.#addJsDocType(this.#targetExpr.varType)} ${this.#targetExpr.name} = `;
 
-    this.#source += `/** @type {${this.#targetExpr.varType}} */ ${this.#targetExpr.name} = `;
+    const prevTargetExpr = this.#targetExpr;
+    this.#targetExpr = this.#targetExpr.body;
+
+    this.#separator = ' ';
+    this.#allowSpaces = false;
+    this.#allowSemi = false;
+
+    this.walk();
+
+    this.#separator = '';
+    this.#allowSpaces = true;
+    this.#allowSemi = true;
+
+    this.#targetExpr = prevTargetExpr;
+
+    this.#source += ';\n';
+  }
+
+  constantDeclaration() {
+    this.#addSpaces();
+
+    this.#source += `const ${this.#targetExpr.name} = `; // `const ${this.#addJsDocType(this.#targetExpr.varType)} ${this.#targetExpr.name} = `;
 
     const prevTargetExpr = this.#targetExpr;
     this.#targetExpr = this.#targetExpr.body;
@@ -175,10 +244,27 @@ export default class Walker {
   setExpression() {
     this.#addSpaces();
 
+    /*
     this.#source += `${this.#targetExpr.name} = `;
+    */
 
-    const prevTargetExpr = this.#targetExpr;
-    this.#targetExpr = this.#targetExpr.body;
+    let prevTargetExpr = this.#targetExpr;
+    this.#targetExpr = this.#targetExpr.left;
+
+    this.#allowSpaces = false;
+    this.#allowSemi = false;
+
+    this.walk();
+
+    this.#allowSpaces = true;
+    this.#allowSemi = true;
+
+    this.#targetExpr = prevTargetExpr;
+
+    this.#source += ' = ';
+
+    prevTargetExpr = this.#targetExpr;
+    this.#targetExpr = this.#targetExpr.right;
 
     this.#separator = ' ';
     this.#allowSpaces = false;
@@ -200,18 +286,27 @@ export default class Walker {
       this.#addSpaces();
     }
 
-    if (Walker.#STDFUNCTIONS.includes(this.#targetExpr.name)) {
-      this.#foundStdFunctions.push(this.#targetExpr.name);
-      this.#targetExpr.name = `__${this.#targetExpr.name}`;
+    const simpleCallName = this.#targetExpr.name.reduce((acc, val) => acc + val?.value ?? '', '');
+
+    if (Walker.#STDFUNCTIONS.includes(simpleCallName)) {
+      this.#foundStdFunctions.push(simpleCallName);
+      this.#targetExpr.name = [ { type: 'Identifier', value: `__${simpleCallName}`, parent: this.#targetExpr } ];
     }
 
     if (this.#targetExpr.async) {
       this.#source += 'await ';
     }
 
-    this.#source += `${this.#targetExpr.name}(`;
+    let prevTargetExpr = this.#targetExpr;
+    this.#targetExpr = this.#targetExpr.name;
 
-    const prevTargetExpr = this.#targetExpr;
+    this.walk();
+
+    this.#targetExpr = prevTargetExpr;
+
+    this.#source += '(';
+    // this.#source += `${this.#targetExpr.name}(`;
+
     this.#targetExpr = this.#targetExpr.body;
 
     const prevAllowSpaces = this.#allowSpaces;
@@ -325,7 +420,61 @@ export default class Walker {
   returnExpression() {
     this.#addSpaces();
 
-    this.#source += `return ${this.#targetExpr.name};\n`;
+    this.#source += 'return ';
+
+    const prevTargetExpr = this.#targetExpr;
+    this.#targetExpr = this.#targetExpr.body;
+
+    this.walk();
+
+    this.#targetExpr = prevTargetExpr;
+
+    this.#source += ';\n';
+  }
+
+  nilExpression() {
+    if (['is', 'not', '===', '!=='].includes(this.#prevTargetExpr.value)) {
+      this.#source = this.#source.slice(0, -2);
+    }
+
+    this.#source += ' null';
+  }
+
+  objectDeclaration() {
+    if (this.#allowSpaces) {
+      this.#addSpaces();
+    }
+
+    this.#source += `{`;
+    
+    if (this.#targetExpr.body.length !== 0 && (this.#targetExpr.body.length - 3) % 4 !== 0) {
+      throw new Error('Wrong amount of arguments for object');
+    }
+
+    if (this.#targetExpr.body.length > 0) {
+      this.#depth++;
+      
+      this.#source += '\n';
+      this.#addSpaces();
+    }
+
+    for (let i = 0; i < this.#targetExpr.body.length; i += 4) {
+      this.#source += `${this.#targetExpr.body[i].value}: ${this.#targetExpr.body[i + 2].value}`;
+
+      if (i + 4 <= this.#targetExpr.body.length) {
+        this.#source += ',\n';
+        this.#addSpaces();
+      }
+    }
+
+    if (this.#targetExpr.body.length > 0) {
+      this.#depth--;
+
+      this.#source += '\n';
+      this.#addSpaces();
+    }
+
+    this.#source += '}';
   }
 
   number() {
@@ -345,22 +494,38 @@ export default class Walker {
   }
 
   identifier() {
+    if (this.#targetExpr.parent.type === 'GlobalsDeclaration') {
+      if (this.#allowSpaces) {
+        this.#addSpaces();
+      }
+
+      this.#source += 'const ';
+    }
+
     this.#source += this.#targetExpr.value;
 
-    if (this.#walkIndex < this.#walkLength - 1 && this.#nextTargetExpr?.value !== '->') {
+    if (this.#walkIndex < this.#walkLength - 1 && this.#nextTargetExpr?.value !== '.' && this.#allowSpaces) {
       this.#source += ' ';
     }
   }
 
   operator() {
+    if (Object.keys(Walker.#NATIVE_OPERATORS).includes(this.#targetExpr.value)) {
+      this.#source += Walker.#NATIVE_OPERATORS[this.#targetExpr.value];
+    } else {
+      this.#source += this.#targetExpr.value;
+    }
+
+    /*
     if (this.#targetExpr.value === '->') {
       this.#source += '.';
       return;
     }
 
     this.#source += this.#targetExpr.value;
+    */
 
-    if (this.#walkIndex < this.#walkLength - 1) {
+    if (this.#walkIndex < this.#walkLength - 1 && this.#targetExpr.value !== '.' && this.#allowSpaces) {
       this.#source += ' ';
     }
   }
@@ -371,6 +536,14 @@ export default class Walker {
     }
 
     this.#source += `// ${this.#targetExpr.body.map(e => e.value ?? '').join(' ')}\n`;
+  }
+
+  debugExpression() {
+    if (this.#allowSpaces) {
+      this.#addSpaces();
+    }
+
+    this.#source += 'debugger;\n';
   }
 
   walk() {
@@ -410,9 +583,5 @@ export default class Walker {
 
   getSource() {
     return this.#source;
-  }
-
-  writeSource() {
-    fs.writeFileSync('./source.js', this.#source, { encoding: 'utf-8' });
   }
 }
