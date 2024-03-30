d086432e5564a546ec1aa0e8253a04080dda1a79
SimonDei <s.f.deitermann@gmail.com>
2024-03-30 18:26:02 +0100

    Create browser.js

---
+++ transpiler/browser.js
@@ -0,0 +1,35 @@
+import Scanner from './tokenizer.js';
+import Parser from './parser.js';
+import Walker from './walker.js';
+
+function interpretScripts() {
+  const scripts = document.querySelectorAll('script[type="text/basescript"]');
+
+  const scanner = new Scanner();
+  const parser = new Parser();
+
+  for (const script of scripts) {
+    const tokens = scanner.tokenize(script.textContent);
+
+    parser.reset(tokens);
+    const { ast } = parser.parse('EOF');
+
+    const walker = new Walker(ast);
+    walker.walk();
+    walker.addStdFunctions();
+
+    const newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.textContent = walker.getSource();
+
+    document.body.append(newScript);
+  }
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', function() {
+    interpretScripts();
+  });
+} else {
+  interpretScripts();
+}
