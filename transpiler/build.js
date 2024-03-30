bf17ee1aa58c14f93209f5f7fdfc2fcd2f40bd6e
SimonDei <s.f.deitermann@gmail.com>
2024-03-30 18:25:37 +0100

    Update build.js

--- transpiler/build.js
+++ transpiler/build.js
@@ -3,16 +3,18 @@ import { build } from 'esbuild';
 build({
   entryPoints: ['browser.js'],
   bundle: true,
-  target: 'es2020',
+  target: 'es2022',
   format: 'iife',
+  drop: ['console', 'debugger'],
   outfile: 'dist/basescript.js'
 });
 
 build({
   entryPoints: ['browser.js'],
   bundle: true,
-  target: 'es2020',
+  target: 'es2022',
   format: 'iife',
+  drop: ['console', 'debugger'],
   minify: true,
   outfile: 'dist/basescript.min.js'
 });
