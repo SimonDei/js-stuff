d66de45379719a76f98781b0b965898e6bd859d7
SimonDei <s.f.deitermann@gmail.com>
2024-04-21 21:09:17 +0200

    Update stdfunctions.js

--- asm/stdfunctions.js
+++ asm/stdfunctions.js
@@ -1,5 +1,5 @@
 const MnemonicDecls = {};
-const StdDecls = {};
+const StdDecls = { call: {}, invoke: {} };
 
 // ========================================================================================
 // Interval Mnenomics
@@ -10,15 +10,15 @@ MnemonicDecls.mov = function(dest, source) {
 };
 
 MnemonicDecls.push = function(value) {
-  __stack.push(value)
+  __stack.push(value);
 };
 
 MnemonicDecls.call = function(func) {
   func();
 };
 
-MnemonicDecls.dec = function(dest) {
-  __writeRegister(dest, __readRegister(dest) - 1);
+MnemonicDecls.jmp = function(funcName, ...args) {
+  __scope[funcName].call(__scope, ...args);
 };
 
 MnemonicDecls.sub = function(dest, source) {
@@ -26,15 +26,53 @@ MnemonicDecls.sub = function(dest, source) {
 };
 
 MnemonicDecls.invoke = function(funcName, ...args) {
-  __scope[funcName].call(__scope, ...args);
+  typeof funcName === 'string'
+    ? __scope[funcName].call(__scope, ...args)
+    : funcName.call(this, ...args);
+};
+
+MnemonicDecls.sizestr = function(value) {
+  return value.length;
+};
+
+MnemonicDecls.substr = function(start, end) {
+  return value.substring(start, end);
+};
+
+MnemonicDecls.catstr = function(str1, str2) {
+  return str1 + str2;
+};
+
+MnemonicDecls.echo = function(value) {
+  console.log(value);
 };
 
 // ========================================================================================
 // Standard Functions
 // ========================================================================================
 
-StdDecls.log = function() {
+StdDecls.call.log = function() {
   console.log(__stack.pop());
+};
+
+StdDecls.call.querySelector = function() {
+  __writeRegister('r1', document.querySelector(__stack.pop()));
+};
+
+StdDecls.invoke.log = function(...values) {
+  console.log(...values);
+};
+
+StdDecls.invoke.QuerySelector = function(selector) {
+  __writeRegister('r1', document.querySelector(selector));
+};
+
+StdDecls.invoke.SetAttribute = function(name, value) {
+  __readRegister('r1').setAttribute(name, value);
+};
+
+StdDecls.invoke.SetProperty = function(name, value) {
+  __readRegister('r1')[name] = value;
 };
 
 export { MnemonicDecls, StdDecls };
