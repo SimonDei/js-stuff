b190d6d48813ccf6540d40cbd7fbc1ed9c21aa30
SimonDei <s.f.deitermann@gmail.com>
2025-01-13 22:23:51 +0100

    Update tmpl.js

--- tmpl.js
+++ tmpl.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
 const builtins = {
   HTML: {
+    Include: function(str) {
+      return compile(parse(fs.readFileSync(path.join(__dirname, 'views', str), 'utf8')));
+    },
     Encode: function(val) {
       return val.toString().replace(/[&<>"'/]/g, m => {
         return '&#' + m.charCodeAt(0) + ';';
@@ -11,35 +17,28 @@ const builtins = {
       return val.toString();
     }
   },
-  /**
-   * Returns a Boolean value that indicates whether a specified variable has been initialized or not
-   */
-  IsEmpty: function(val) {
-    if (typeof val === 'object') {
-      return Object.keys(val).length === 0;
-    }
-    return val === undefined || val === null || val === '';
-  },
-  /**
-   * Returns a Boolean value that indicates whether a specified expression contains no valid data (Null)
-   */
-  IsNull: function(val) {
-    return val === null;
-  },
-  /**
-   * Returns a Boolean value that indicates whether a specified expression can be evaluated as a number
-   */
-  IsNumeric: function(val) {
-    return !isNaN(val);
-  },
-  /**
-   * Returns the number of characters in a string or the number of elements in an array.
-   */
-  Len: function(val) {
-    return Array.isArray(val) || typeof val === 'string' ? val.length : 0;
-  },
+  // ========== Date/Time Functions ========== //
   Date: function() {
-    return Intl.DateTimeFormat('de-DE').format(new Date());
+    return Intl.DateTimeFormat('de-DE', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    }).format(new Date());
+  },
+  Day: function(str) {
+    return new Date(str).getDate();
+  },
+  Hour: function(str) {
+    return new Date(str).getHours();
+  },
+  IsDate: function(str) {
+    return !isNaN(Date.parse(str));
+  },
+  Minute: function(str) {
+    return new Date(str).getMinutes();
+  },
+  Month: function(str) {
+    return new Date(str).getMonth() + 1;
   },
   Now: function() {
     return Intl.DateTimeFormat('de-DE', {
@@ -51,95 +50,136 @@ const builtins = {
       second: '2-digit'
     }).format(new Date());
   },
-  /**
-   * Returns a variant containing an array
-   */
+  Second: function(str) {
+    return new Date(str).getSeconds();
+  },
+  Time: function() {
+    return Intl.DateTimeFormat('de-DE', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    }).format(new Date());
+  },
+  Weekday: function(str) {
+    return new Date(str).getDay() + 1;
+  },
+  WeekdayName: function(str) {
+    return new Date(str).toLocaleDateString('de-DE', {
+      weekday: 'long'
+    });
+  },
+  Year: function(str) {
+    return new Date(str).getFullYear();
+  },
+  // ========== Conversion Functions ========== //
+  Asc: function(val) {
+    return val.charCodeAt(0);
+  },
+  Chr: function(val) {
+    return String.fromCharCode(val);
+  },
+  // ========== Math Functions ========== //
+  Abs: function(val) {
+    return Math.abs(val);
+  },
+  Hex: function(val) {
+    return val.toString(16);
+  },
+  Int: function(val) {
+    return Number.parseInt(val);
+  },
+  Oct: function(val) {
+    return val.toString(8);
+  },
+  Rnd: function() {
+    return Math.random();
+  },
+  Sqr: function(val) {
+    return Math.sqrt(val);
+  },
+  // ========== Array Functions ========== //
   Array: function(...vals) {
     return [...vals];
   },
-  /**
-   * Returns a Boolean value that indicates whether a specified variable is an array
-   */
+  Filter: function(str, needle, include = true) {
+    return str.filter(item => include ? item.includes(needle) : !item.includes(needle));
+  },
   IsArray: function(val) {
     return Array.isArray(val);
   },
-  /**
-   * Removes spaces on both the left and the right side of a string
-   */
-  Trim: function(val) {
-    return val.trim();
+  Join: function(arr, delimiter = " ") {
+    return arr.join(delimiter);
   },
-  /**
-   * Removes spaces on the left side of a string
-   */
-  LTrim: function(val) {
-    return val.trimStart();
+  LBound: function(arr) {
+    return 0;
   },
-  /**
-   * Removes spaces on the right side of a string
-   */
-  RTrim: function(val) {
-    return val.trimEnd();
-  },
-  /**
-   * Returns a specified number of characters from the left side of a string
-   */
-  Left: function(val, len) {
-    return val.slice(0, len);
-  },
-  /**
-   * Returns a specified number of characters from the right side of a string
-   */
-  Right: function(val, len) {
-    return val.slice(-len);
-  },
-  /**
-   * Returns a specified number of characters from a string
-   */
-  Mid: function(val, start, len) {
-    return val.slice(start, start + len);
-  },
-  /**
-   * Returns a string that contains a repeating character of a specified length
-   */
-  String: function(len, char) {
-    return char.repeat(len);
-  },
-  /**
-   * Reverses a string
-   */
-  StrReverse: function(val) {
-    return val.split('').reverse().join('');
-  },
-  /**
-   * Converts a specified string to uppercase
-   */
-  UCase: function(val) {
-    return val.toUpperCase();
-  },
-  /**
-   * Converts a specified string to lowercase
-   */
-  LCase: function(val) {
-    return val.toLowerCase();
-  },
-  /**
-   * Rounds a number
-   */
-  Round: function(val, decimals) {
-    return Number(val.toFixed(decimals));
-  },
-  /**
-   * Returns a zero-based, one-dimensional array that contains a specified number of substrings
-   */
   Split: function(val, delimiter = " ", count = -1) {
     return val.split(delimiter, count);
   },
-  /**
-   * Returns a string that consists of a number of substrings in an array
-   */
-  Join: function(arr, delimiter = " ") {
-    return arr.join(delimiter);
+  UBound: function(arr) {
+    return arr.length - 1;
+  },
+  // ========== String Functions ========== //
+  InStr: function(val, needle, start = 1) {
+    return val.indexOf(needle, start - 1) + 1;
+  },
+  InStrRev: function(val, needle, start = -1) {
+    return val.lastIndexOf(needle, start - 1) + 1;
+  },
+  LCase: function(val) {
+    return val.toLowerCase();
+  },
+  Left: function(val, len) {
+    return val.slice(0, len);
+  },
+  Len: function(val) {
+    return val.length;
+  },
+  LTrim: function(val) {
+    return val.trimStart();
+  },
+  RTrim: function(val) {
+    return val.trimEnd();
+  },
+  Trim: function(val) {
+    return val.trim();
+  },
+  Mid: function(val, start, len) {
+    return val.slice(start, start + len);
+  },
+  Replace: function(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+  },
+  Right: function(val, len) {
+    return val.slice(-len);
+  },
+  StrComp: function(str1, str2) {
+    return str1.localeCompare(str2);
+  },
+  String: function(len, char) {
+    return char.repeat(len);
+  },
+  StrReverse: function(val) {
+    return val.split('').reverse().join('');
+  },
+  UCase: function(val) {
+    return val.toUpperCase();
+  },
+  // ========== Other Functions ========== //
+  IsEmpty: function(val) {
+    if (typeof val === 'object') {
+      return Object.keys(val).length === 0;
+    }
+    return val === undefined || val === null || val === '';
+  },
+  IsNull: function(val) {
+    return val === null;
+  },
+  IsNumeric: function(val) {
+    return !isNaN(val);
+  },
+  Round: function(val, decimals) {
+    return Number(val.toFixed(decimals));
   },
   ObjectKeys: function(obj) {
     return Object.keys(obj);
@@ -159,17 +199,15 @@ const escapeExprRegexp = /<%=\s*([\s\S]+?)\s*%>/gi;
 const exprRegexp = /<%\s*(?<expression>[\s\S]+?)\s*%>/gi;
 
 /**
- * @param {string} template
- * @param {object} data
+ * @param {string} source The template source code
  * @returns {string}
  */
-export function parseTemplate(template, data = {}) {
-  const combinedData = { ...data, ...builtins };
-
-  template = template
-    .replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g," ")
-    .replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g,"")
+export function parse(source) {
+  return source
+    .replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g, " ")
+    .replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g, "")
     .replace(/'|\\/g, "\$&")
+    .replace(/''/gi, '\\\'\\\'')
     .replace(dimRegexp, (_, varname, expression) => {
       return `';(${varname.trim()} = ${expression.trim()});out+='`;
     })
@@ -208,7 +246,23 @@ export function parseTemplate(template, data = {}) {
     .replace(/\t/g, '\\t')
     .replace(/\r/g, "\\r")
 		.replace(/(\s|;|\}|^|\{)out\+='';/g, '$1')
-    .replace(/\+''/g, "");
-
-  return new Function('data', "let out = '';with(data){out = '" + template + "'}return out;")(combinedData);
 }
+
+/**
+ * @param {string} template The parsed template code
+ * @param {object} data
+ * @returns {string}
+ */
+export function compile(template, data = {}) {
+  data = { ...data, ...builtins };
+
+  return new Function('data', "let out = '';with(data){out = '" + template + "'}return out;")(data);
+}
+
+/*
+async function viewAsp(page, data, opts) {
+  data = Object.assign({}, defaultCtx, this.locals, data)
+  page = await getTemplate(getPage(page, 'asp'));
+  return engine.parseTemplate(page, data)
+}
+*/
