23f8c0ab6a5e08c30ea3bedd5efa30e81c0f7c8b
SimonDei <s.f.deitermann@gmail.com>
2025-05-20 15:30:26 +0200

    Update index.js

--- mini-c/index.js
+++ mini-c/index.js
@@ -1,23 +1,20 @@
-import Scanner from './tokenizer-2.js';
-import Parser from './parser-2.js';
-import Walker from './walker.js';
+import Scanner from './tokenizer.js';
+import Parser from './parser.js';
+// import Walker from './walker.js';
 import * as fs from 'fs';
-import * as util from 'util';
 
-const userArgs = process.argv.slice(2);
-let typesEnabled = true;
+//const userArgs = process.argv.slice(2);
+//let typesEnabled = true;
 
-if (userArgs.length > 0) {
-  if (userArgs.includes('--types')) {
-    typesEnabled = true;
-  }
-}
+//if (userArgs.length > 0) {
+//  if (userArgs.includes('--types')) {
+//    typesEnabled = true;
+//  }
+//}
 
-const code = fs.readFileSync('./index.txt', { encoding: 'utf-8' });
+const code = fs.readFileSync('./index.c', { encoding: 'utf-8' });
 
 const tokens = new Scanner().tokenize(code);
-
-// console.log(tokens);
 
 fs.writeFileSync('./tokens.json', '[\n' + tokens.reduce((acc, tok) => acc + `{ "type": "${tok.type}", "value": "${tok.value}", "line": ${tok.line} },\n`, '') + ']\n', { encoding: 'utf-8' });
 
