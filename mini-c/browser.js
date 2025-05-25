60c2bbfa4899b4f77b912cf6a0bb848ea0af3cd7
SimonDei <s.f.deitermann@gmail.com>
2025-05-25 22:20:03 +0200

    Create browser.js

---
+++ mini-c/browser.js
@@ -0,0 +1,31 @@
+import { parseCode } from './chevro-parser.js';
+import { astBuilder } from './ast-builder.js';
+import CodeGenerator from './code-generator.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scripts = document.querySelectorAll('script[type="text/mini-c"]');
+  for (const script of scripts) {
+    try {
+      const { cst, lexErrors, parseErrors } = parseCode(script.textContent);
+      
+      if (lexErrors.length > 0) {
+        console.error("Lexer Fehler:", lexErrors);
+      }
+      if (parseErrors.length > 0) {
+        console.error("Parser Fehler:", parseErrors);
+      }
+      
+      if (lexErrors.length === 0 && parseErrors.length === 0) {
+        const ast = astBuilder.visit(cst);
+        const generator = new CodeGenerator();
+        const generatedCode = ast.body.map(node => generator.generate(node)).join('\n\n');
+        const scriptElement = document.createElement('script');
+        scriptElement.type = 'text/javascript';
+        scriptElement.textContent = generatedCode;
+        document.body.appendChild(scriptElement);
+      }
+    } catch (error) {
+      console.error("Ein Fehler ist aufgetreten:", error.message);
+    }
+  }
+});
